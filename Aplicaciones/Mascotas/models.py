70a9061b532ce86f3a6695511db2c075c284756f
roderiiich <r.cortescartes@gmail.com>
2025-05-18 23:23:26 -0400

    ultimo commit

--- Aplicaciones/Mascotas/models.py
+++ Aplicaciones/Mascotas/models.py
@@ -13,6 +13,9 @@ class Dueño(models.Model):
 class Especie_Mascota(models.Model):
     id=models.AutoField(primary_key=True,max_length=6)
     nombre_especie=models.CharField(max_length=50)
+    def __str__(self):
+        texto= "{0} (ID: {1})"
+        return texto.format(self.nombre_especie,self.id)
 
 class Mascota(models.Model):
     id=models.AutoField(primary_key=True ,unique=True)
